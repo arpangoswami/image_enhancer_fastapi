28ceee042982b6cc3391240308fdbdd9d692b38f
Arpan Goswami <arpang777@gmail.com>
2022-05-19 10:11:56 +0530

    Added cors middleware

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from image_processing.image_fusion import get_fused_image
 
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+]
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ImageMerger(BaseModel):
-    encoded_img1: str
-    encoded_img2: str
+    grayscale_img: str
+    blur_img: str
 
 
 @app.get("/")
@@ -21,5 +32,5 @@ def health_check():
 @app.post("/merge_two_images")
 def merge_two_images(images: ImageMerger):
     return {
-        "merged_image": get_fused_image(images.encoded_img1, images.encoded_img2)
+        "merged_image": get_fused_image(images.grayscale_img, images.blur_img)
     }
